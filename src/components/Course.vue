de779e0f29ad5016e9c7ee75a17150f9c2a46d06
amokrunner <danny@amokrunner.com>
2019-05-01 19:55:17 -0700

    Update

--- src/components/Course.vue
+++ src/components/Course.vue
@@ -2,14 +2,11 @@
   <div class="container-fluid mt-4">
     <h1 class="h1">{{ course.name }}</h1>
     <b-alert :show="loading" variant="info">Loading...</b-alert>
+    <b-card>
+      <CourseChart :points="points" :user="user" />
+    </b-card>
     <div role="tablist">
       <b-card no-body class="mb-1">
-        <b-card-header header-tag="header" class="p-1" role="tab">
-          <b-button block href="#" v-b-toggle.accordion-0 variant="info">Chart</b-button>
-        </b-card-header>
-        <b-collapse id="accordion-0" visible accordion="my-accordion" role="tabpanel">
-          <CourseChart :points="points" :user="user" />
-        </b-collapse>
         <b-card-header header-tag="header" class="p-1" role="tab">
           <b-button block href="#" v-b-toggle.accordion-1 variant="info">Splits</b-button>
         </b-card-header>
