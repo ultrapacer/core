94bfc55989c6f65a806fabae2988a479606ad017
Danny Murphy <danny@amokrunner.com>
2019-05-15 20:17:08 -0700

    closes #39

--- src/components/WaypointTable.vue
+++ src/components/WaypointTable.vue
@@ -1,5 +1,5 @@
 <template>
-  <b-table :items="waypoints" :fields="fields" primary-key="_id" hover small>
+  <b-table :items="waypoints" :fields="fields" primary-key="_id" @row-clicked="toggleRowDetails" hover small>
     <template slot="actions" slot-scope="row">
       <b-button size="sm" @click="editFn(row.item)" class="mr-2">
         <v-icon name="edit"></v-icon><span class="d-none d-md-inline">Edit</span>
@@ -8,6 +8,18 @@
         <v-icon name="trash"></v-icon><span class="d-none d-md-inline">Delete</span>
       </b-button>
     </template>
+    <template slot="row-details" slot-scope="row">
+      <b-card>
+        <b-row class="mb-2">
+          <b-button size="sm" class="mr-2" variant="outline-primary">&lt;&lt;&lt;</b-button>
+          <b-button size="sm" class="mr-2" variant="outline-primary">&lt;&lt;</b-button>
+          <b-button size="sm" class="mr-2" variant="outline-primary">&lt;</b-button>
+          <b-button size="sm" class="mr-2" variant="outline-primary">&gt;</b-button>
+          <b-button size="sm" class="mr-2" variant="outline-primary">&gt;&gt;</b-button>
+          <b-button size="sm" class="mr-2" variant="outline-primary">&gt;&gt;&gt;</b-button>
+        </b-row>
+      </b-card>
+      </template>
   </b-table>
 </template>
 
@@ -44,6 +56,12 @@ export default {
         })
       }
       return f
+    }
+  },
+  methods: {
+    toggleRowDetails: function (row) {
+      // To toggle:
+      this.$set(row, '_showDetails', !row._showDetails)
     }
   }
 }
