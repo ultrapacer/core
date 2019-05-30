0820eed77c9beb5731ea0b7cfb0c8ef0cb21a12f
amokrunner <danny@amokrunner.com>
2019-05-30 08:06:39 -0700

    Remove unneeded code

--- server/routes/publicRoutes.js
+++ server/routes/publicRoutes.js
@@ -5,37 +5,6 @@ var Course = require('../models/Course')
 var Waypoint = require('../models/Waypoint')
 var Plan = require('../models/Plan')
 
-// GET COURSE
-publicRoutes.route('/course/:course').get(async function (req, res) {
-  try {
-    var course = await Course.findById(req.params.course).populate(['_gpx', '_plan']).exec()
-    if (course.public) {
-      res.json(course)
-    } else {
-      res.status(403).send('No permission')
-    }
-  } catch (err) {
-    res.status(400).send(err)
-  }
-})
-
-// GET WAYPOINT LIST
-publicRoutes.route('/course/waypoints/:courseID').get(async function (req, res) {
-  try {
-    console.log(req.params.courseID)
-    var course = await Course.findById(req.params.courseID).exec()
-    if (course.public) {
-      var waypoints = await Waypoint.find({ _course: course }).sort('location').exec()
-      res.json(waypoints)
-    } else {
-      res.status(403).send('No permission')
-    }
-  } catch (err) {
-    console.log(err)
-    res.status(400).send(err)
-  }
-})
-
 // GET PLAN LIST
 publicRoutes.route('/course/plans/:courseID').get(async function (req, res) {
   try {
