1d8d7492ad71ad36a40d12ff28f1ad9fda31bf8c
amokrunner <danny@amokrunner.com>
2019-06-05 14:05:59 -0700

    Update PlanEdit.vue

--- src/components/PlanEdit.vue
+++ src/components/PlanEdit.vue
@@ -154,10 +154,11 @@ export default {
         this.model._course = this.course._id
         p = await api.createPlan(this.model)
       }
-      await this.$emit('refresh', p)
-      this.saving = false
-      this.clear()
-      this.$bvModal.hide('plan-edit-modal')
+      await this.$emit('refresh', p, () => {
+        this.saving = false
+        this.clear()
+        this.$bvModal.hide('plan-edit-modal')
+      }
     },
     clear () {
       this.model = Object.assign({}, this.defaults)
