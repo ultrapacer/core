19ac4324ee991d4b19cb8159f3566b70b5270f5c
amokrunner <danny@amokrunner.com>
2019-06-07 19:34:04 -0700

    Update utilities.js

--- shared/utilities.js
+++ shared/utilities.js
@@ -130,6 +130,7 @@ function cleanPoints (points) {
 
 function addLoc (p) {
   var d = 0
+  var t0 = 0, t1=0, t2=0,t3=0,st01=0,st12=0, st23=0
   p[0].loc = 0
   for (var i = 1, il = p.length; i < il; i++) {
     d += (gpxParse.utils.calculateDistance(p[i - 1].lat, p[i - 1].lon, p[i].lat, p[i].lon))
@@ -138,7 +139,9 @@ function addLoc (p) {
   for (i = 0, il < p.length; i < il; i++) {
     var a2s = 0
     var w = 0
+    t0 = performance.now()
     var p2 = p.filter(x => x.loc >= p[i].loc - 0.1 && x.loc <= p[i].loc + 0.100)
+    t1 = performance.now()
     var xyr = []
     // need to update this to make sure at least one other point is selected 
     // ahead and behind
@@ -150,74 +153,23 @@ function addLoc (p) {
       console.log(i)
       console.log(xyr)
     }
+    t2 = performance.now()
     var ab = linear_regression(xyr)
+    t3 = performance.now()
     p[i].grade = round(ab[0] / 10, 2)
     if (p[i].grade > 50) { p[i].grade = 50 }
     else if (p[i].grade < -50) { p[i].grade = -50 }
+    st01 += t1 - t0
+    st12 += t2 - t1
+    st23 += t3 - t2
   }
+  console.log('st01 ' + st01)
+  console.log('st12 ' + st12)
+  console.log('st23 ' + st23)
   return p
 }
 
-function wls(values_x, values_y, weights) {
-    var sum_x = 0;
-    var sum_y = 0;
-    var sum_xy = 0;
-    var sum_xx = 0;
-    var count = 0;
 
-    /*
-     * We'll use those variables for faster read/write access.
-     */
-    var x = 0;
-    var y = 0;
-    var values_length = values_x.length;
-
-    if (values_length != values_y.length) {
-        throw new Error('The parameters values_x and values_y need to have same size!');
-    }
-
-    /*
-     * Nothing to do.
-     */
-    if (values_length === 0) {
-        return [ [], [] ];
-    }
-
-    /*
-     * Calculate the sum for each of the parts necessary.
-     */
-    for (var v = 0; v < values_length; v++) {
-        x = values_x[v];
-        y = values_y[v];
-        sum_x += x;
-        sum_y += y;
-        sum_xx += x*x;
-        sum_xy += x*y;
-        count++;
-    }
-
-    /*
-     * Calculate m and b for the formular:
-     * y = x * m + b
-     */
-    var m = (count*sum_xy - sum_x*sum_y) / (count*sum_xx - sum_x*sum_x);
-    var b = (sum_y/count) - (m*sum_x)/count;
-
-    /*
-     * We will make the x and y result line now
-     */
-    var result_values_x = [];
-    var result_values_y = [];
-
-    for (var v = 0; v < values_length; v++) {
-        x = values_x[v];
-        y = x * m + b;
-        result_values_x.push(x);
-        result_values_y.push(y);
-    }
-
-    return { xs: result_values_x, ys: result_values_y}
-}
 
 function linear_regression( xyr )
 {
