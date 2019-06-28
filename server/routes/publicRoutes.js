a5c1371c4ea6016cd2a9e930928e2e9f51ea9644
amokrunner <danny@amokrunner.com>
2019-06-27 20:45:29 -0700

    Fix public course
    closes #100

--- server/routes/publicRoutes.js
+++ server/routes/publicRoutes.js
@@ -2,16 +2,13 @@
 var express = require('express')
 var publicRoutes = express.Router()
 var Course = require('../models/Course')
-var Waypoint = require('../models/Waypoint')
-var Plan = require('../models/Plan')
 
 // GET COURSE
 publicRoutes.route('/course/:course').get(async function (req, res) {
   try {
-    var course = await Course.find({ _id: req.params.course }).populate(['_plan']).exec()
+    var q = { _id: req.params.course }
+    var course = await Course.findOne(q).populate(['_plan']).exec()
     if (course.public) {
-      course.waypoints = await Waypoint.find({ _course: course }).sort('location').exec()
-      course.plans = await Plan.find({ _course: course }).sort('name').exec()
       res.json(course)
     } else {
       res.status(403).send('No permission')
