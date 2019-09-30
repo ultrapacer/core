077cdcd9694965fb221a1689e66286f69b672e07
amokrunner <danny@amokrunner.com>
2019-09-30 08:10:35 -0700

    Update planRoutes.js

--- server/routes/planRoutes.js
+++ server/routes/planRoutes.js
@@ -24,7 +24,7 @@ planRoutes.route('/:id').put(async function (req, res) {
     var plan = await Plan.findById(req.params.id).exec()
     if (user.equals(plan._user)) {
       let fields = ['name', 'description', 'pacingMethod', 'pacingTarget',
-        'drift', 'startTime', 'heatModel', 'waypointDelay']
+        'drift', 'startTime', 'heatModel', 'waypointDelay', 'eventStart', 'eventTimezone']
       fields.forEach(f => {
         plan[f] = req.body[f]
       })
