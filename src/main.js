75901128a9f4b243409ccc6fc9ea111fb4ee46af
amokrunner <danny@amokrunner.com>
2019-10-09 05:35:52 -0700

    Update main.js

--- src/main.js
+++ src/main.js
@@ -15,11 +15,12 @@ import VueTheMask from 'vue-the-mask'
 import { Icon } from 'leaflet'
 import 'leaflet/dist/leaflet.css'
 
-import 'vue-awesome/icons/trash'
-import 'vue-awesome/icons/edit'
-import 'vue-awesome/icons/plus'
-import 'vue-awesome/icons/lock'
 import 'vue-awesome/icons/download'
+import 'vue-awesome/icons/edit'
+import 'vue-awesome/icons/facebook'
+import 'vue-awesome/icons/lock'
+import 'vue-awesome/icons/plus'
+import 'vue-awesome/icons/trash'
 import VIcon from 'vue-awesome/components/Icon'
 delete Icon.Default.prototype._getIconUrl
 Icon.Default.mergeOptions({
