4b74eea7a45f77219ae9ff04d672842748d54f92
amokrunner <danny@amokrunner.com>
2019-10-14 19:42:34 -0700

    A start

---
+++ src/components/AboutContent.vue
@@ -0,0 +1,65 @@
+<template>
+<div>
+  <b-card no-body class="overflow-hidden" style="max-width: 540px;">
+    <b-row no-gutters>
+      <b-col md="6">
+        <b-card-img src="https://picsum.photos/400/400/?image=20" class="rounded-0"></b-card-img>
+      </b-col>
+      <b-col md="6">
+        <b-card-body title="Create Courses">
+          <b-card-text>
+            Upload GPS tracks to create courses and define waypoints (aid stations, landmarks, junctions, etc.) along the way.
+          </b-card-text>
+        </b-card-body>
+      </b-col>
+    </b-row>
+  </b-card>
+
+
+  <b-card no-body class="overflow-hidden" style="max-width: 540px;">
+    <b-row no-gutters>
+      <b-col md="6">
+        <b-card-img src="https://picsum.photos/400/400/?image=20" class="rounded-0"></b-card-img>
+      </b-col>
+      <b-col md="6">
+        <b-card-body title="Plan Your Pacing">
+          <b-card-text>
+            Strategize pace splits for your upcoming ultramarathon or mountain adventure. Pacing plans factor grade, altitude, terrain, heat, darkness, and fatigue.
+          </b-card-text>
+        </b-card-body>
+      </b-col>
+    </b-row>
+  </b-card>
+
+  <b-card no-body class="overflow-hidden" style="max-width: 540px;">
+    <b-row no-gutters>
+      <b-col md="6">
+        <b-card-img src="https://picsum.photos/400/400/?image=20" class="rounded-0"></b-card-img>
+      </b-col>
+      <b-col md="6">
+        <b-card-body title="Aid Station/Landmark Arrival Times">
+          <b-card-text>
+            Determine when you should expect to arrive at aid station or landmarks along your course so your crew knows when to expect you. 
+          </b-card-text>
+        </b-card-body>
+      </b-col>
+    </b-row>
+  </b-card>
+
+  <b-card no-body class="overflow-hidden" style="max-width: 540px;">
+    <b-row no-gutters>
+      <b-col md="6">
+        <b-card-img src="https://picsum.photos/400/400/?image=20" class="rounded-0"></b-card-img>
+      </b-col>
+      <b-col md="6">
+        <b-card-body title="Course Analysis">
+          <b-card-text>
+             Break down course segments or distance splits by time, grade, pace, ascent/descent, distance, and other metrics.
+          </b-card-text>
+        </b-card-body>
+      </b-col>
+    </b-row>
+  </b-card>
+
+</div>
+<template>
