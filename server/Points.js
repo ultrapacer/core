a685d530047d405b4fb2e7936bf467aa02cadd7f
amokrunner <danny@amokrunner.com>
2019-10-18 19:42:57 -0700

    Create Points.js

---
+++ server/Points.js
@@ -0,0 +1,15 @@
+var mongoose = require('mongoose')
+var Schema = mongoose.Schema
+
+// Define collection and schema for Posts
+var PointsSchema = new Schema({
+  _course: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Course'
+  },
+  data: [Array],
+}, {
+  collection: 'points'
+})
+
+module.exports = mongoose.model('Points', PointsSchema)
