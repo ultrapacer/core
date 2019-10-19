8788692f506c5691605302d2acbd2f425359e48f
amokrunner <danny@amokrunner.com>
2019-10-19 13:56:03 -0700

    Update publicRoutes.js

--- server/routes/publicRoutes.js
+++ server/routes/publicRoutes.js
@@ -46,7 +46,10 @@ publicRoutes.route('/:course/points').get(async function (req, res) {
       public: true
     }
     var course = await Course.findOne(q).select('points').exec()
-    res.json(course.points)
+    let p = course.points.map(x => {
+      return [x.lat, x.lon, x.alt]
+    })
+    res.json(p)
   } catch (err) {
     console.log(err)
     res.status(400).send(err)
