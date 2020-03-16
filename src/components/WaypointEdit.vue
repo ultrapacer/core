241105e1e5ba9354e16a08c1e61ce05742c6433f
amokrunner <danny@amokrunner.com>
2020-03-16 12:58:03 -0700

    Add GA events to Waypoint create/edit/delete (#248)

--- src/components/WaypointEdit.vue
+++ src/components/WaypointEdit.vue
@@ -209,9 +209,11 @@ export default {
       }
       wputil.updateLLA(this.model, this.points)
       if (this.model._id) {
+        this.$ga.event('Waypoint', 'edit', this.course.public ? this.course.name : 'private')
         await api.updateWaypoint(this.model._id, this.model)
       } else {
         this.model._course = this.course._id
+        this.$ga.event('Waypoint', 'delete', this.course.public ? this.course.name : 'private')
         await api.createWaypoint(this.model)
       }
       this.$emit('refresh', () => {
