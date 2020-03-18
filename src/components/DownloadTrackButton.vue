bd9ed5033fd5fc239a6dfe3b7371cf1637f811e6
Danny Murphy <danny@amokrunner.com>
2020-03-18 08:58:00 -0700

    Add TCX Export. Closes #232

---
+++ src/components/DownloadTrackButton.vue
@@ -0,0 +1,19 @@
+<template>
+  <b-button
+    @click="!ready ? $emit('generate') : {}"
+    :href="ready ? url : ''"
+    :download="ready ? filename : ''"
+    :variant="ready ? 'primary' : 'outline-primary'"
+    :disabled="disabled"
+  >
+    <v-icon v-show="ready" name="download"></v-icon>
+    <b-spinner v-show="spinner" small></b-spinner>
+    {{ ready ? 'Download' : 'Generate' }} {{ type }} ({{ resolution }} Resolution)
+  </b-button>
+</template>
+
+<script>
+export default {
+  props: ['ready', 'spinner', 'url', 'filename', 'type', 'resolution', 'disabled']
+}
+</script>
