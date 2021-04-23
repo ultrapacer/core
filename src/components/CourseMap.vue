fc437c392fb3aa41c78e187bc9f4a2118bd270b1
Danny Murphy <danny@amokrunner.com>
2021-04-23 12:38:59 -0700

    Change to https for ESRI Satellite map

--- src/components/CourseMap.vue
+++ src/components/CourseMap.vue
@@ -127,7 +127,7 @@ export default {
           name: 'ESRI Satellite',
           'max-zoom': 18,
           visible: false,
-          url: 'http://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}',
+          url: 'https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}',
           attribution:
             'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
         }
