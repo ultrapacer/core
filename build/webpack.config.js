412bfec0c0e0e9c2db683112c40f10f0bfd7e555
amokrunner <danny@amokrunner.com>
2022-02-26 05:43:18 -0800

    progress

--- build/webpack.config.js
+++ build/webpack.config.js
@@ -49,17 +49,15 @@ const config = {
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader'
-        }
+        use: ['babel-loader']
       },
       {
         test: /\.vue$/,
-        loader: 'vue-loader'
+        use: ['vue-loader']
       },
       {
         test: /\.md$/,
-        loader: 'vue-loader!vue-md-loader'
+        use: ['vue-loader', 'vue-md-loader']
       },
       {
         test: /\.s?css$/,
@@ -78,17 +76,16 @@ const config = {
         ]
       },
       {
-        test: /\.(eot|ttf|woff|woff2)(\?\S*)?$/,
-        loader: 'file-loader'
-      },
-      {
         test: /\.(png|jpe?g|gif|webm|mp4|svg)$/,
-        loader: 'file-loader',
-        options: {
-          name: '[name].[ext]',
-          outputPath: 'public/img',
-          esModule: false
+        use: [{
+          loader: 'file-loader',
+          options: {
+            name: '[name].[ext]',
+            outputPath: 'public/img',
+            esModule: false
+          }
         }
+        ]
       },
       {
         test: /\.(js|vue)$/,
@@ -103,6 +100,12 @@ const config = {
     ]
   },
   plugins: [
+    new webpack.ProvidePlugin({
+      Buffer: ['buffer', 'Buffer']
+    }),
+    new webpack.ProvidePlugin({
+      process: 'process/browser'
+    }),
     new BoostrapVueLoader(),
     new VueLoaderPlugin(),
     new MiniCssExtractPlugin({
@@ -123,12 +126,29 @@ const config = {
   resolve: {
     alias: {
       vue$: 'vue/dist/vue.esm.js',
-      '@': resolve('src')
+      '@': resolve('src'),
+      'process/browser': 'process/browser.js' // this fixes issue with html2pdf.js
     },
-    extensions: ['*', '.js', '.vue', '.json']
+    extensions: ['*', '.js', '.vue', '.json'],
+    fallback: {
+      fs: false,
+      tls: false,
+      net: false,
+      path: false,
+      zlib: false,
+      http: false,
+      stream:  require.resolve('stream/'),  // this one is used by xml2js for creation of gpx files
+      crypto: false,
+      os: false,
+      assert: false,
+      url: false,
+      timers: false,
+      https: false,
+      util: require.resolve('util/')
+    }
   },
   optimization: {
-    moduleIds: 'hashed',
+    //    moduleIds: 'hashed',  // depreciated
     runtimeChunk: 'single',
     splitChunks: {
       chunks: 'all'
@@ -148,17 +168,17 @@ const config = {
         target: 'http://localhost:8080'
       }
     }
-  },
-  node: {
-    fs: 'empty'
   }
+  // node: {
+  //  fs: 'empty'
+  // }
 }
-module.exports = (env, argv) => {
-  if (argv.mode === 'development') {
+module.exports = (env) => {
+  if (env.mode === 'development') {
     config.devtool = 'eval-cheap-module-source-map'
 
     // if running in development with front end only, proxy the api to actual server
-    if (argv.serverless) {
+    if (env.serverless) {
       Object.assign(
         config.devServer.proxy['/api'],
         {
@@ -168,25 +188,30 @@ module.exports = (env, argv) => {
       )
     }
   }
-  if (argv.mode === 'production') {
+  if (env.mode === 'production') {
     config.output.filename = 'public/js/[name].[contenthash:8].js'
     config.output.chunkFilename = 'public/js/[name].[contenthash:8].js'
     config.plugins.push(
-      new CopyWebpackPlugin([
-        {
-          from: path.resolve(__dirname, '../static'),
-          to: path.resolve(__dirname, '../dist/public'),
-          ignore: ['.*']
-        },
-        // copy over rendered web component javascript:
-        {
-          from: path.resolve(__dirname, '../temp'),
-          to: path.resolve(__dirname, '../dist/public/components'),
-          ignore: ['.*', '*.html']
-        }
-      ])
+      new CopyWebpackPlugin({
+        patterns: [
+          {
+            from: path.resolve(__dirname, '../static'),
+            to: path.resolve(__dirname, '../dist/public'),
+            globOptions: {
+              ignore: ['.*']
+            }
+          },
+          // copy over rendered web component javascript:
+          {
+            from: path.resolve(__dirname, '../temp'),
+            to: path.resolve(__dirname, '../dist/public/components'),
+            globOptions: {
+              ignore: ['.*', '*.html']
+            }
+          }
+        ]
+      })
     )
-
     // compile sitemap with all documentation:
     const paths = ['', 'races', 'docs']
     const docs = require('../src/docs/.config').docs
@@ -202,7 +227,7 @@ module.exports = (env, argv) => {
         }
       })
     )
-    if (argv.analyzer === 'on') {
+    if (env.analyzer === 'on') {
       config.plugins.push(
         new BundleAnalyzerPlugin()
       )
--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
+// import { BootstrapVue } from 'bootstrap-vue'
 import router from './router'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
@@ -44,6 +45,9 @@ import './custom.scss'
 import time from './util/time'
 import core from '../core'
 import math from '../core/math'
+
+// Vue.use(BootstrapVue)
+
 Vue.prototype.$core = core
 Vue.prototype.$math = math
 
