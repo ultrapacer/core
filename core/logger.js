21d2f3e9c0d11ec8ac67474b7ba17b08aca83a11
amokrunner <danny@amokrunner.com>
2022-03-25 12:08:24 -0700

    Adjust error reporting. Fixes #395

--- core/logger.js
+++
@@ -1,8 +0,0 @@
-// this is obsolete and just used until all project transitions to winston
-const winston = require('winston')
-function logger (message) {
-  winston.verbose('use of core/logger.js is depreciated')
-  if (message) return winston.info(message)
-  return false
-}
-exports.logger = logger
