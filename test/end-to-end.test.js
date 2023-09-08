b7666f4ed9a4c0f4183d759779aa2763765a6f39
Danny <144149847+amokrunnr@users.noreply.github.com>
2023-09-08 09:11:06 -0700

    Add vitest and initial end-to-end tests (#435)

---
+++ test/end-to-end.test.js
@@ -0,0 +1,16 @@
+import _ from 'lodash'
+import { expect, test } from 'vitest'
+import { course, plan } from './data'
+
+test('check track distance', () => {
+  expect(_.round(course.track.dist, 5)).toBe(159.32537)
+})
+
+test('Elapsed time at the end of segment 16', () => {
+  expect(_.round(plan.splits.segments[16].point2.elapsed, 1)).toBe(75798.7)
+})
+
+test('Check overall paces', () => {
+  expect(_.round(plan.pacing.np, 2)).toBe(377.70)
+  expect(_.round(plan.pacing.pace, 2)).toBe(468.45)
+})
