41c7412f109c6e0194a577df7ec95bec18267f50
amokrunnr <danny.l.murphy@gmail.com>
2023-09-08 12:03:57 -0700

    eslintrc update

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,9 +4,7 @@ require('@rushstack/eslint-patch/modern-module-resolution')
 module.exports = {
   root: true,
   extends: [
-    'plugin:vue/recommended',
     'eslint:recommended',
-    '@vue/eslint-config-prettier/skip-formatting'
   ],
   parserOptions: {
     ecmaVersion: 'latest'
